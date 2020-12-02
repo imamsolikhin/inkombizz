7aff45aa5236191ac8cb792efea61b085be85288
imamsolikhin <imamsolikhin@github.com>
2020-12-02 14:53:17 +0700

    up

---
+++ api-ikb-mst/cmd.sh
@@ -0,0 +1,4 @@
+app="api_asset_noc"
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=${app} --format="{{.ID}}"))
+docker build -t ${app} .
+docker run -i -d -p 8014:8014 --name=${app} -v $PWD:/app ${app}
